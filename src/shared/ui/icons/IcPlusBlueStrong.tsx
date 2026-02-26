380d6d7af688376dab2fbb17c6229ba389812a8e
kwakseongjae <gkffhdnls13@gmail.com>
2026-02-26 18:18:44 +0900

    [#169] Chore: IcPlusBlueStrong 아이콘 에셋 추가
    Closes #169

---
+++ src/shared/ui/icons/IcPlusBlueStrong.tsx
@@ -0,0 +1,21 @@
+import type { SVGProps } from 'react';
+
+const SvgIcPlusBlueStrong = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={9}
+    height={9}
+    fill="none"
+    viewBox="0 0 9 9"
+    aria-hidden="true"
+    {...props}
+  >
+    <path
+      stroke="#3182F6"
+      strokeLinecap="round"
+      strokeWidth={1.5}
+      d="M.75 4.387h7.273M4.387.75v7.273"
+    />
+  </svg>
+);
+export default SvgIcPlusBlueStrong;
